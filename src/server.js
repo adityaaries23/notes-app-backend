21fd4b5b6ab5a809fec6d221f84e2b7dcd1724b0
aditya.aries <102455608+adityaaries23@users.noreply.github.com>
2023-07-02 07:21:49 +0700

    implement hapi plugin

--- src/server.js
+++ src/server.js
@@ -1,18 +1,29 @@
-const Hapi = require("@hapi/hapi");
-const routes = require("./routes");
+const Hapi = require('@hapi/hapi');
+const routes = require('./api/notes/routes');
+const NotesService = require('./services/inMemory/notesService');
+const notes = require('./api/notes');
 
 const init = async () => {
+  const noteService = new NotesService();
+
   const server = Hapi.server({
-    port: "5000",
-    host: process.env.NODE_ENV !== "production" ? "localhost" : "0.0.0.0",
+    port: '5000',
+    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
     routes: {
       cors: {
-        origin: ["http://notesapp-v1.dicodingacademy.com"],
+        origin: ['http://notesapp-v1.dicodingacademy.com'],
       },
     },
   });
 
-  server.route(routes);
+  // server.route(routes);
+
+  await server.register({
+    plugin: notes,
+    options: {
+      service: noteService,
+    },
+  });
 
   await server.start();
 
